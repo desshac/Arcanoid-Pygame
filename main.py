3d6db53206d1db88376f7685c7cda7a76a84b14a
jerkiii <gmaxim753@gmail.com>
2023-01-10 15:07:08 +0500

    new design

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import pygame
@@ -12,25 +13,25 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Arcanoid')
 # fon color
 startScreen_color = ("#DDBEAA")
-bg = ("#DDBEAA")
+bg = ("#1950A0")
 # blocks color
-block_red = ("#BBC6C8")
-block_green = ("#5BA199")
-block_blue = ("#469597")
+block_red = ("#F63232")
+block_green = ("#58B822")
+block_blue = ("#2681EB")
 platform_color = ("#E5E3E4")
 # shadow color
-red_shadow = ("#839295")
-green_shadow = ("#4A847E")
-blue_shadow = ("#367B7D")
+red_shadow = ("#BC3838")
+green_shadow = ("#3E8317")
+blue_shadow = ("#255996")
 platform_shadow = ("#C1C1C1")
 
 cols = 6
 rows = 6
 
 map_count = 0
-count = 0
+score = 0
 
-FPS = 60
+FPS = int(screen_height / 10 + 20)
 clock = pygame.time.Clock()
 
 game_over = None
@@ -41,24 +42,34 @@ def terminate():
     sys.exit()
 
 
+def load_image(name):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print("Can't load image")
+        terminate()
+    image = pygame.image.load(fullname)
+    return image
+
+
 def start_screen():
-    intro_text = ["Arcanoid", "Начать игру"]
-    screen.fill(startScreen_color)
-    font = pygame.font.Font(None, 50)
-    text_coord = 50
+    intro_text = ["Arcanoid Pygame", "Начать игру", 'Выход']
+    font = pygame.font.Font(None, 60)
+    text_coord = 80
     text_x = screen_width // 2
     button = None
+    button2 = None
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
         intro_rect.top = text_coord
         intro_rect.x = text_x - (intro_rect.width // 2)
         font = pygame.font.Font(None, 40)
-        text_coord += 100
         screen.blit(string_rendered, intro_rect)
+        text_coord += 100
         if intro_text.index(line) == 1:
             button = intro_rect
+        elif intro_text.index(line) == 2:
+            button2 = intro_rect
 
     while True:
         for event in pygame.event.get():
@@ -68,6 +79,9 @@ def start_screen():
                 if button.right >= event.pos[0] >= button.x:
                     if button.bottom >= event.pos[1] >= button.y:
                         return
+                if button2.right >= event.pos[0] >= button2.x:
+                    if button2.bottom >= event.pos[1] >= button2.y:
+                        terminate()
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -75,24 +89,23 @@ def start_screen():
 
 def end_screen():
     if game_over == 2:
-        intro_text = ["Вы выиграли!", f"Ваш счет {count}", "Выход в главное меню", "Выход"]
+        intro_text = ["Вы выиграли!", f"Ваш счет {score}", "Выход в главное меню", "Выход"]
     elif game_over == -1:
-        intro_text = ["Вы проиграли!", f"Ваш счет {count}", "Выход в главное меню", "Выход"]
+        intro_text = ["Вы проиграли!", f"Ваш счет: {score}", "Выход в главное меню", "Выход"]
 
-    screen.fill(startScreen_color)
+    # screen.fill(startScreen_color)
     font = pygame.font.Font(None, 50)
-    text_coord = 50
+    text_coord = screen_height / 2 - 80
     text_x = screen_width // 2
     button, button2 = None, None
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
         intro_rect.top = text_coord
         intro_rect.x = text_x - (intro_rect.width // 2)
         font = pygame.font.Font(None, 40)
-        text_coord += 100
         screen.blit(string_rendered, intro_rect)
+        text_coord += 50
         if intro_text.index(line) == 2:
             button = intro_rect
         elif intro_text.index(line) == 3:
@@ -188,7 +201,7 @@ class wall():
 
 class platform:
     def __init__(self):
-        self.pl_width = 200
+        self.pl_width = 150
         self.pl_height = 30
         self.pl_x = screen_width // 2 - self.pl_width // 2
         self.pl_y = screen_height - 60
@@ -233,7 +246,7 @@ class ball:
                            self.ball_radius)
 
     def update_ball(self):
-        global count
+        global score
         collision = 7
         row_count = 0
         for row in wall.blocks:
@@ -255,8 +268,7 @@ class ball:
                         wall.blocks[row_count][item_count][0] = pygame.Rect(0, 0, 0, 0)
                         wall.blocks[row_count][item_count][1] = pygame.Rect(0, 0, 0, 0)
                         wall.blocks_count -= 1
-                        count += item[2]
-                    print(count)
+                        score += item[2]
 
                 item_count += 1
             row_count += 1
@@ -264,9 +276,9 @@ class ball:
         font = pygame.font.Font(None, 40)
         text = 'Нажмите пробел чтобы начать'
         if not ball_running:
-            font = font.render(text, 1, pygame.Color('black'))
+            font = font.render(text, 1, pygame.Color('white'))
             key = pygame.key.get_pressed()
-            screen.blit(font, (100, 100))
+            screen.blit(font, (100, screen_height / 2 - 20))
             if key[pygame.K_RIGHT] and self.rect.right < screen_width - 5:
                 self.rect = self.rect.move(self.speed, 0)
             if key[pygame.K_LEFT] and self.rect.left > 5:
@@ -306,7 +318,7 @@ class ball:
             return self.game_over
 
 
-level_map_list = load_level('lvl1.txt', 'lvl2.txt')
+level_map_list = load_level('lvl1.txt', 'lvl3.txt', 'lvl2.txt')
 
 wall = wall()
 platform = platform()
@@ -314,19 +326,29 @@ platform = platform()
 ball = ball(platform.pl_x + (platform.pl_width // 2), platform.pl_y - platform.pl_height)
 
 while True:
+    image = load_image('fon.bmp')
+    image = pygame.transform.scale(image, (screen_width, screen_height))
+
+    screen.blit(image, (0, 0))
+    platform.draw_platform()
+    ball.draw_ball()
+
     start_screen()
+
     map_count = len(level_map_list) - 1
     wall.create_wall(level_map_list)
 
     game_over = 0
+    score = 0
 
     ball_running = False
     run = True
     counter_font = pygame.font.Font(None, 30)
     while run:
-        text = f'Ваш счет: {count}'
-        counter = counter_font.render(text, 1, pygame.Color('black'))
-        screen.fill(bg)
+        wall.draw_wall()
+        text = f'Ваш счет: {score}'
+        counter = counter_font.render(text, 1, pygame.Color('white'))
+        screen.blit(image, (0, 0))
         wall.draw_wall()
         platform.draw_platform()
         ball.draw_ball()
